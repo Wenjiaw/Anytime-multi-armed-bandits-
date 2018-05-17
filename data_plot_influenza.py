444f39dc19c5795d0fc712f10627b95343677ef7
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-05-17 15:30:18 +0200

    Add files via upload

---
+++ data_plot_influenza.py
@@ -0,0 +1,90 @@
+import csv
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from environments.influenza import preventive_means
+
+class Plot():
+    def __init__(self, m, steps,n,R_0):
+        self.m = m
+        self.n = n
+        self.steps =steps
+        self.R_0 = R_0
+
+    def read_data(self,data):
+        means_value = preventive_means(self.R_0)
+        result = np.zeros((2, self.steps))
+        with open(data,"r") as csvfile:
+                 reader = csv.reader(csvfile)
+                 count = 0
+                 for row in reader:
+                     try:
+                        top_m = [means_value[int(i)] for i in row]
+                        result[0][count] = np.min(top_m)
+                        result[1][count] = np.sum(top_m)
+                        count = count + 1
+
+                     except:
+                        pass
+        return result
+
+    def get_means(self,data):
+        result = np.zeros((2, self.steps))
+        for i in range(1,self.n+1):
+            J_t_value = self.read_data('%s/outfile_%i.csv'%(data,i))
+            result = result + J_t_value
+        result = result / self.n
+        return result
+
+    def plot(self):
+        dsar_data = "output_%s_DSAR_%i" % (self.R_0,self.m)
+        naivedsar_data = "output_%s_naive_DSAR_%i" % (self.R_0,self.m)
+        uniform_data = "output_%s_Uniform_%i" % (self.R_0,self.m)
+        ATLUCB_data = "output_%s_AT_LUCB_%i" % (self.R_0,self.m)
+        naivedsar = self.get_means(naivedsar_data)
+        uniform = self.get_means(uniform_data)
+        dsar = self.get_means(dsar_data)
+        ATLUCB = self.get_means(ATLUCB_data)
+        plt.figure(1)
+        plt.title('R_0 = %s m = %i min' % (self.R_0, self.m), fontweight='bold')
+        plt.xlabel('t')
+        plt.ylabel('min')
+        plt.plot(ATLUCB[0], 'b-', color='g', label='AT_LUCB')
+        plt.plot(naivedsar[0], 'b-', color='y', label="naive_DSAR")
+        plt.plot(uniform[0], 'b-', color='b', label='uniform')
+        plt.plot(dsar[0], 'b-', color='r', label='DSAR')
+        plt.legend()
+        plt.xlim(0, steps)
+        plt.ylim(0.3, 0.9)
+        plt.savefig('R_0 = %s m = %i_min.png' % (self.R_0, self.m))
+        plt.figure(2)
+        plt.title('R_0 = %s m = %i sum' % (self.R_0, self.m), fontweight='bold')
+        plt.xlabel('t')
+        plt.ylabel('sum')
+        plt.plot(ATLUCB[1], 'b-', color='g', label='AT_LUCB')
+        plt.plot(naivedsar[1], 'b-', color='y', label="naive_DSAR")
+        plt.plot(uniform[1], 'b-', color='b', label='Uniform')
+        plt.plot(dsar[1], 'b-', color='r', label='DSAR')
+        plt.legend()
+        plt.xlim(0, self.steps)
+        plt.ylim(36.0, 44.0)
+        plt.savefig('R_0 = %s m = %i_sum.png' % (self.R_0, self.m),format='png')
+
+
+
+
+
+m=4
+steps = 150000
+R_0 = 1.4
+n = 200
+
+plot = Plot(m,steps,n,R_0).plot()
+
+
+
+
+
+
+
