aa1c9248da1b1347f872193273b367ac093a88ae
Pieter Libin <pieter.libin@vub.ac.be>
2018-05-17 14:37:35 +0200

    comments

--- algorithms/dsar.py
+++ algorithms/dsar.py
@@ -2,32 +2,40 @@ import numpy as np
 import bottleneck
 import math
 
+#PL: just have one DSAR file, that you can configure with a Boolean?
 
 class DSAR:
     def __init__(self, bandit, m):
         self.bandit = bandit
         self.m = m
+        #same comments as for at-lucb
         self.reward_perarm = [[] for i in range(len(self.bandit.arms))]
         self.mean_estimates = np.full(len(self.bandit.arms), float(0))
+
         self.active = np.arange(1 ,len(self.bandit.arms)+1 , 1)
-        self.Jt = np.random.choice(len(self.bandit.arms),self.m)
+        #self.Jt = np.random.choice(len(self.bandit.arms),self.m)
+
+        #PL: again, no history here
         self.J = []  # history of top m
         self.J.append(self.Jt)
+        
         self.s = 1
         self.k = 1
-        self.m_lack = self.m
-        self.n = [0 if k == 0 else 1 for k in range(len(self.bandit.arms))]
-        self.count = self.n[self.k] - self.n[self.k - 1]
-        self.i = 0
 
-    def initial_stage(self):
-        self.m_lack = self.m
-        self.s = self.s + 1
-        budget = 2 ** (self.s - 1) * len(self.bandit.arms)
-        self.n = [math.ceil(self.n_k(k, budget)) for k in range(len(self.bandit.arms))]
-        self.active = np.arange(1, len(self.bandit.arms) + 1, 1)
-        self.k = 1
-        self.count = self.n[self.k] - self.n[self.k-1]
+        self.phase_counter = 0
+        #self.m_lack = self.m
+        #self.n = [0 if k == 0 else 1 for k in range(len(self.bandit.arms))]
+        #self.count = self.n[self.k] - self.n[self.k - 1]
+        #self.i = 0
+
+    #def initial_stage(self):
+    #    self.m_lack = self.m
+    #    self.s = self.s + 1
+    #    budget = 2 ** (self.s - 1) * len(self.bandit.arms)
+    #    self.n = [math.ceil(self.n_k(k, budget)) for k in range(len(self.bandit.arms))]
+    #    self.active = np.arange(1, len(self.bandit.arms) + 1, 1)
+    #    self.k = 1
+    #    self.count = self.n[self.k] - self.n[self.k-1]
 
     def n_k(self, k, budget):
         if k == 0:
@@ -35,9 +43,11 @@ class DSAR:
         else:
             K = len(self.bandit.arms)
             n = budget
-            return 1/self.log(K) * (n-k)/(K+1-k)
+            return 1/self.log_(K) * (n-k)/(K+1-k)
 
-    def log(self, n):
+    #use fractions here, only convert to float when returing,
+    #to avoid rounding errors
+    def log_(self, n):
         a = 1 / 2
         for i in range(2, n + 1):
             a = a + 1 / i
@@ -47,20 +57,22 @@ class DSAR:
         self.reward_perarm[arm_i].append(reward)
         self.mean_estimates[arm_i] = np.mean(self.reward_perarm[arm_i])
 
-    def empirical_gaps(self):
-        if self.m_lack == 0:
-            pass
-        else:
-            mean_active = self.mean_estimates[self.active-1]
-            index = np.argsort(-mean_active)
-            if mean_active[index[0]] - mean_active[index[self.m_lack]] >= (
-                mean_active[index[self.m_lack - 1]] - mean_active[index[-1]]):
-                self.Jt[self.m - self.m_lack] = self.active[index[0]] - 1
-                self.m_lack = self.m_lack - 1
-                self.active = np.delete(self.active, index[0])
-            else:
-                self.active = np.delete(self.active, index[-1])
-        return self.active
+    #TODO: define a gap function n terms of the sigma bijection
+    
+    #def empirical_gaps(self):
+    #    if self.m_lack == 0:
+    #        pass
+    #    else:
+    #        mean_active = self.mean_estimates[self.active-1]
+    #        index = np.argsort(-mean_active)
+    #        if mean_active[index[0]] - mean_active[index[self.m_lack]] >= (
+    #            mean_active[index[self.m_lack - 1]] - mean_active[index[-1]]):
+    #            self.Jt[self.m - self.m_lack] = self.active[index[0]] - 1
+    #            self.m_lack = self.m_lack - 1
+    #            self.active = np.delete(self.active, index[0])
+    #        else:
+    #            self.active = np.delete(self.active, index[-1])
+    #    return self.active
 
     def get_top_m(self):
         if self.m_lack == 0:
@@ -75,21 +87,25 @@ class DSAR:
     def step(self, t):
         reward = self.bandit.play(self.active[self.i] - 1)
         self.add_reward(self.active[self.i] - 1, reward)
-        self.Jt = self.get_top_m()
-        if self.i == len(self.active) - 1:
-            self.count = self.count - 1
-            self.i = 0
-            if self.count == 0:
-                while self.count == 0:
-                    self.active = self.empirical_gaps()
-                    self.i = 0
-                    if self.k < len(self.bandit.arms) - 1:
-                        self.k = self.k + 1
-                        self.count = self.n[self.k] - self.n[self.k - 1]
-                    else:
-                        J_laststage = np.copy(self.Jt)
-                        self.J.append(J_laststage)
-                        self.initial_stage()
-        else:
-            self.i = self.i + 1
-        return self.Jt
+
+        self.phase_counter = self.phase_counter + 1
+
+        #if we are at the end of the phase
+        if phase_counter > (n_k(k) - n_k(k-1)):
+            #1. decide which arm to reject
+            max_ = sys.float_info.min
+            for a in active:
+                g = gap(a, n_k(k))
+                if g > max_:
+                    max_ = g
+                #TODO: remove max_ from active
+                #    -> as you remove stuff from it,
+                #       active is maybe better a list than an array?
+            #2. go to the next phase
+            self.k = self.k + 1
+
+        #TODO: if all phases are done, go to the next stage
+            
+        
+        
+    #return depending on your DSAR variant that is defined by the Boolean ...
