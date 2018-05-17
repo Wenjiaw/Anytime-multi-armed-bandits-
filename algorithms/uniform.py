e1a86094f781945f100e06a3191adafe22d268eb
Pieter Libin <pieter.libin@vub.ac.be>
2018-05-17 14:41:27 +0200

    comments

--- algorithms/uniform.py
+++ algorithms/uniform.py
@@ -4,6 +4,7 @@ class Uniform:
     def __init__(self, bandit, m):
         self.bandit = bandit
         self.m = m
+        #same remarks as for the at-lucb
         self.reward_perarm = [[] for i in range(len(self.bandit.arms))]
         self.mean_estimates = np.full(len(bandit.arms), float(0))
 
@@ -11,19 +12,20 @@ class Uniform:
         self.reward_perarm[arm_i].append(reward)
         self.mean_estimates[arm_i] = np.mean(self.reward_perarm[arm_i])
 
+    #refactor least_sampled_index
     def leastsample_index(self):
-        if 0 in self.mean_estimates:
-            leastsampled = np.where(self.mean_estimates == 0)[0]
-        else:
-            counts_per_arm = [sum(self.reward_perarm[i])/self.mean_estimates[i] for i in range(len(self.bandit.arms))]
-            leastsampled = np.where(counts_per_arm == np.min(counts_per_arm))[0]
+        counts_per_arm = #just take the lenghts of rewards_per_arm ?
+        least_sampled = np.where(counts_per_arm == np.min(counts_per_arm))[0]
+        
         return leastsampled
 
     def step(self, t):
-        leastsampled = self.leastsample_index()
-        arm_i = np.random.choice(leastsampled)
+        least_sampled = self.leastsample_index()
+        arm_i = np.random.choice(least_sampled)
+        
         reward = self.bandit.play(arm_i)
         self.add_reward(arm_i, reward)
+        
         J_t = bottleneck.argpartition(-self.mean_estimates, self.m)[:self.m]
         return J_t
 
