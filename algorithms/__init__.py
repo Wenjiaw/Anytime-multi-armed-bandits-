753df108e4e7610da2d2e93e027d514a7cbb70c7
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-06-19 13:18:34 +0200

    Add files via upload

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,8 +1,9 @@
 
 
 from algorithms.atlucb import *
-from algorithms.dsar_fixed_fluctuation import *
 from algorithms.dsar_naive import *
 from algorithms.dsh_best_arm import *
 from algorithms.uniform import *
 from algorithms.dsar import *
+from algorithms.ucb import *
+from algorithms.thompson_sampling import *
