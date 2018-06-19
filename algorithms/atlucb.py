d812bdea478afb8cdebbd281c660a1acaf393f72
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-06-19 13:13:35 +0200

    Add files via upload

--- algorithms/atlucb.py
+++ algorithms/atlucb.py
@@ -1,8 +1,6 @@
 import numpy as np
-import bottleneck
-import random
+import sys
 class AT_LUCB:
-    #PL: the d symbol is not a sigma but a delta?
     
     def __init__(self, sigma1, alpha, epsilon ,bandit, m):
         self.sigma1 = sigma1
@@ -10,44 +8,25 @@ class AT_LUCB:
         self.bandit = bandit
         self.epsilon = epsilon
         self.m = m
-        #PL: refactor rewards_per_arm
         self.reward_per_arm = [[] for i in range(len(bandit.arms))]
-        #PL: refactor mean_per_arm
         self.mean_per_arm = np.full(len(bandit.arms), float(0))
+        self.Jt = np.full(self.m, -1)
+        self.S = [1]
 
-        #PL: why intialize Jt to a random number?
-        #why not to -1, it is never used until it is set in the algo right?
-        self.Jt = np.random.randint(len(bandit.arms), size=self.m)
-
-        #PL:
-        #no need to keep this history here,
-        #it is not part of the algo, if you want to keep it,
-        #keep it where you call the algorithm !
-        # -> decouple: only have the algo stuff here!
-        self.J = [] # history of top m
-        self.J.append(self.Jt)
-
-        #you don't need a list? just S_t?
-        self.S = [1] #list of stages, initially 1
-
-    #use this function
     def sigma(self, s):
-        return sigma1 * alpha**(s-1)
+        return self.sigma1 * self.alpha**(s-1)
 
     def beta(self, u, t, sigma1):
         k1 = 1.25
         n = len(self.bandit.arms)
-        return (np.log(n)*k1*((t)**4)/sigma1)/(2*u))**0.5
+        return ((np.log(n*k1*(t**4)/sigma1))/(2*u))**0.5
 
-    #PL:
-    #determines whether the terminal condition for this stage is satisfied
-    #  -> correct?
     def term(self, t, sigma, epsilon):
-        h = h(t, sigma)
-        l = l(t, sigma)
-        U = U(t, l, sigma)
-        T = T(t, h, sigma)
-        return U - T < epsilon
+        h = self.h(t, sigma) #max
+        l = self.l(t, sigma) #min
+        U = self.U(t, l, sigma)
+        L = self.L(t, h, sigma)
+        return U - L < epsilon
         
     def L(self, t, a, sigma):
         mu = self.mean_per_arm[a]
@@ -65,60 +44,37 @@ class AT_LUCB:
 
     def h(self, t, sigma):
         min_ = sys.float_info.max
-        for j in self.J_t:
-            L = L(t, j, sigma)
+        min_index = -1
+        for j in self.Jt:
+            L = self.L(t, j, sigma)
             if L < min_:
                 min_ = L
-        return min_
+                min_index = j
+
+        return int(min_index)
 
     def l(self, t, sigma):
         max_ = sys.float_info.min
-        for j in range(len(bandit.arms))
-            if j in self.J_t:
+        max_index = -1
+        for j in range(len(self.bandit.arms)):
+            if j in self.Jt:
                 pass
             else:
-                U = U(t, j, sigma)
+                U = self.U(t, j, sigma)
                 if U > max_:
                     max_ = U
-        return max_
-                
-                
-        
-
-    def high_arm(self, t): #return the mini  mean -beta  of top m and its index
-        mini_list = [True if self.mean_estimates[i] == 0 else (self.mean_per_arm[i] - self.beta(np.sum(self.reward_per_arm[i])/self.mean_per_arm[i], t-1, self.sigma)) for i in self.J[t-1]]
-        # mini_list store the mean_estimates - Beta of the top m arms.
-        # store the ones nerve been pulled as True (beta is infinite)
-        if True in mini_list:
-            mini = True
-        else:
-            mini = np.min(mini_list)
-        index = self.J[t-1][mini_list.index(mini)]
-        return index, mini
-
-
-    def low_arm(self,t):#return the max  mean + beta  of top m and its index
-        maxm_list = [True if self.mean_per_arm[i]==0 else (self.mean_per_arm[i] + self.beta(np.sum(self.reward_per_arm[i]) / self.mean_per_arm[i], t-1, self.sigma)) for i in range(len(self.mean_per_arm))]
-        # maxm_list store the mean_estimates + Beta of the all the arms.
-        # store the ones nerve been pulled as True (beta is infinite)
-        for i in range(self.m):
-            maxm_list[(self.J[t-1])[i]] = 0
-        #keep the mean_estimates + beta of top m as 0 to make sure it will not been pulled
-        if True in maxm_list:
-            maxm = True
-        else:
-            maxm = np.max(maxm_list)
-        index = maxm_list.index(maxm)
-        return index, maxm
+                    max_index = j
+        return int(max_index)
 
     def get_top_m(self):
-        return bottleneck.argpartition(-self.mean_per_arm, self.m)[:self.m]
+        return np.argsort(-self.mean_per_arm)[0:self.m]
 
     def add_reward(self, arm_i, reward):
         self.reward_per_arm[arm_i].append(reward)
         self.mean_per_arm[arm_i] = np.mean(self.reward_per_arm[arm_i])
 
     def step(self, t):
+
         if self.term(t, self.sigma(self.S[t - 1]),self.epsilon):
             s = self.S[t - 1]
             while self.term(t, s, self.epsilon):
@@ -129,33 +85,12 @@ class AT_LUCB:
             self.S.append(self.S[t-1])
             if self.S[t] == 1:
                 self.Jt = self.get_top_m()
-        high_index = self.high_arm(t)[0]
-        low_index = self.low_arm(t)[0]
-        highreward = self.bandit.play(high_index)
-        self.add_reward(high_index, highreward)
-        #pull H
-        lowreward = self.bandit.play(low_index)
-        self.add_reward(low_index, lowreward)
-        #pull L
-        self.J.append(self.Jt)
+        if t%2 == 0:
+            lowest_index = self.l(t, self.sigma(self.S[t - 1]))
+            low_reward = self.bandit.play(lowest_index)
+            self.add_reward(lowest_index, low_reward)
+        else:
+            highest_index = self.h(t, self.sigma(self.S[t - 1]))
+            high_reward = self.bandit.play(highest_index)
+            self.add_reward(highest_index, high_reward)
         return self.Jt
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
