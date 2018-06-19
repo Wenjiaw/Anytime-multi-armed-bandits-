325faab6d7c7e8d72a377dacfbf414baad483722
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-06-19 13:03:16 +0200

    Add files via upload

---
+++ algorithms/thompson_sampling.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+class TS:
+    def __init__(self, bandit, m, rank, posterior):
+        self.bandit = bandit
+        self.m = m
+        self.posterior = posterior
+        self.rank = rank
+        self.reward_per_arm = [[] for i in range(len(self.bandit.arms))]
+        self.mean_per_arm = np.full(len(bandit.arms), float(0))
+    def add_reward(self, arm_i, reward):
+        self.reward_per_arm[arm_i].append(reward)
+        self.mean_per_arm[arm_i] = np.mean(self.reward_per_arm[arm_i])
+
+    def get_top_m(self):
+        return np.argsort(-self.mean_per_arm)[0:self.m]
+
+    def step(self, t):
+        if t < len(self.bandit.arms) +1:
+            reward = self.bandit.play(t-1)
+            self.add_reward(t-1, reward)
+        else:
+            sample_result = np.zeros(len(self.reward_per_arm))
+            for i in range(len(self.reward_per_arm)):
+                sample_result[i] = self.posterior.sample_arm_i(self.reward_per_arm[i])
+            order = np.argsort(-np.array(sample_result))
+            arm_i = self.rank(order)
+            reward = self.bandit.play(arm_i)
+            self.add_reward(arm_i, reward)
+
+        self.Jt = self.get_top_m()
+        return self.Jt
