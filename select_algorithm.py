9e91e31726981c249c0940120e457c6ee4f8c5ec
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-06-19 13:20:40 +0200

    Add files via upload

--- select_algorithm.py
+++ select_algorithm.py
@@ -1,11 +1,27 @@
 from algorithms import AT_LUCB
 from algorithms import Uniform
-from algorithms import NAIVE_DSAR
 from algorithms import DSAR
-from environments.gaussian_jun import linear_bandit,polynomial_bandit
-from environments.captions_jun import captions_bandit
-from environments.influenza import preventive_bandit
-def select_algorithm(arm_n, m, variance, bandit,algorithm,R_0):
+from algorithms import UCB
+from algorithms import DSH
+from algorithms import TS
+from environments.gaussian_jun import linear_means,linear_bandit,polynomial_means,polynomial_bandit
+from environments.captions_jun import captions_means,captions_bandit
+from environments.influenza import preventive_means,preventive_bandit
+from posteriors.Gaussion_Posterior import GaussianPosterior
+from ranking import uniform_and_zipf,mth_and_mplus1s
+
+def select_means(arm_n,bandit,R_0):
+    if bandit == "linear":
+        means_value = linear_means(arm_n)
+    elif bandit == "capition":
+        means_value = captions_means()
+    elif bandit == "influenza":
+        means_value = preventive_means(R_0)
+    else:
+        means_value = polynomial_means(arm_n)
+    return means_value
+
+def select_bandit(arm_n,variance, bandit,R_0):
     if bandit == "linear":
         bandits = linear_bandit(arm_n, variance)
     elif bandit == "capition":
@@ -14,13 +30,27 @@ def select_algorithm(arm_n, m, variance, bandit,algorithm,R_0):
         bandits = preventive_bandit(variance,R_0)
     else:
         bandits = polynomial_bandit(arm_n, variance)
+    return bandits
+
+def select_algorithm(arm_n,variance,m,bandits,algorithm,distribution_method):
 
     if algorithm == "AT_LUCB":
         algo = AT_LUCB(0.5, 0.99, 0, bandits, m)
     elif algorithm == "DSAR":
-        algo = DSAR(bandits, m)
+        algo = DSAR(bandits, m, True)
     elif algorithm == "naive_DSAR":
-        algo = NAIVE_DSAR(bandits, m)
+        algo = DSAR(bandits, m, False)
+    elif algorithm == "UCB":
+        algo = UCB(bandits, m)
+    elif algorithm == "DSH":
+        algo = DSH(bandits, m)
     elif algorithm == "Uniform":
         algo = Uniform(bandits, m)
+    elif algorithm == "Thompson":
+        if distribution_method == "uniform_and_zipf":
+            rank = uniform_and_zipf(4,m,arm_n)
+        elif distribution_method =="mth_and_mplus1s":
+            rank = mth_and_mplus1s(m)
+        posterior = GaussianPosterior(variance)
+        algo = TS(bandits, m, rank, posterior)
     return algo
