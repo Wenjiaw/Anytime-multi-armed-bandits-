3dca94f78cdaea365582f0acaa12d5dfc15ce242
Pieter Libin <pieter.libin@vub.ac.be>
2018-06-21 16:04:14 +0200

    keep the hyperparams fixed

--- Gaussion_Posterior.py
+++ Gaussion_Posterior.py
@@ -1,21 +1,17 @@
 import numpy as np
 class GaussianPosterior():
-    def __init__(self, variance):
+    def __init__(self, variance, var0, mu0):
         self.known_var = variance
+        self.var0 = var0
+        self.mu0 = mu0
 
     def sample_arm_i(self,reward):
         n = len(reward)
-        if n == 1:
-            stddev =  np.sqrt(self.known_var)
-            return np.random.normal(reward[0],stddev)
-        else:
-            var0 = np.var(reward)
-            u0 = np.mean(reward)
-            denom = (1.0 / var0 + n / self.known_var)
-            u = (u0 / var0 + sum(reward) / self.known_var) / denom
-            var = 1.0 / denom
-            stddev = np.sqrt(var)
-            return np.random.normal(u,stddev)
+        denom = (1.0 / var0 + n / self.known_var)
+        u = (u0 / var0 + sum(reward) / self.known_var) / denom
+        var = 1.0 / denom
+        stddev = np.sqrt(var)
+        return np.random.normal(u,stddev)
 
     # def mean(self, posterior_per_arm):
     #     mean = np.zeros(len(posterior_per_arm))
