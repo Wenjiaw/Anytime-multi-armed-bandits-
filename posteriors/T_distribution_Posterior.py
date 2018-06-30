f2eb17e16afff27741605d5b9b4e23110ae7f510
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-06-30 02:31:43 +0200

    Add files via upload

--- posteriors/T_distribution_Posterior.py
+++ posteriors/T_distribution_Posterior.py
@@ -1,12 +1,15 @@
 import numpy as np
 class T_Posterior():
 
-    def sample_arm_i(reward):
+    def __init__(self,alpha):
+        self.alpha = alpha
+
+    def sample_arm_i(self,reward):
         n = len(reward)
         if n == 1:
             T = reward[0]
         else:
             sigma = np.std(reward)
             mu = np.mean(reward)
-            T = mu + np.random.standard_t(n-1)*sigma
+            T = mu + np.random.standard_t(n+(2*self.alpha)-1)*sigma
         return T
