7f9a141369b5b6eeb8a32d5c285d2c718372a49c
Pieter Libin <pieter.libin@vub.ac.be>
2018-07-02 12:33:03 +0200

    use the known var, refactor reward into rewards

--- posteriors/truncated_normal_Posterior.py
+++ posteriors/truncated_normal_Posterior.py
@@ -2,15 +2,16 @@ import numpy as np
 from scipy import stats
 
 class truncated_normal():
-    def __init__(self, a, b):
+    def __init__(self, a, b, known_var):
         self.a = a
         self.b = b
+        self.known_var = known_var
 
-    def sample_arm_i(self,reward):
-        n = len(reward)
+    def sample_arm_i(self,rewards):
+        n = len(rewards)
         if n == 0:
             return stats.uniform.rvs(loc=self.a, scale=self.b)
         else:
-            mu0 = np.mean(reward)
-            sigma0 = np.sqrt(np.var(reward)/n)
+            mu0 = np.mean(rewards)
+            sigma0 = np.sqrt(known_var/n)
             return stats.truncnorm.rvs(self.a, self.b, loc=mu0, scale=sigma0)
