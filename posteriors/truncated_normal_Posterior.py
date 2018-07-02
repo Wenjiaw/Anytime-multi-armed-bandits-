78e7136b80acd4a4f600b3f0e60190223a17a808
Pieter Libin <pieter.libin@vub.ac.be>
2018-07-02 10:50:56 +0200

    fix truncated normal posteriors

--- posteriors/truncated_normal_Posterior.py
+++ posteriors/truncated_normal_Posterior.py
@@ -8,16 +8,11 @@ class truncated_normal():
 
     def sample_arm_i(self,reward):
         n = len(reward)
-        mu = np.mean(reward)
-        var = np.var(reward)/n
-        sigma = np.sqrt(var)
-        if n == 1:
-            a = self.a
-            b = self.b
+        mu0 = np.mean(reward)
+        sigma0 = np.var(reward)/n
+        if n == 0:
+            return stats.uniform.rvs(loc=self.a, scale=self.b)
         else:
-            a = (self.a - mu) / sigma
-            b = (self.b - mu) / sigma
-        result = stats.truncnorm.rvs(a, b, loc=mu, scale=sigma)
-        return result
-
-
+            mu0 = np.mean(reward)
+            sigma0 = np.var(reward)/n
+            return stats.truncnorm.rvs(self.a, self.b, loc=mu0, scale=sigma0)
