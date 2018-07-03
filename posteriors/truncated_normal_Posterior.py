9a5a938a2ad7f1393e0edc24862baa07c323b367
wenjia wang <wenjia.wang0315@gmail.com>
2018-07-03 17:47:12 +0200

    a,b normlized

--- posteriors/truncated_normal_Posterior.py
+++ posteriors/truncated_normal_Posterior.py
@@ -1,26 +1,30 @@
+
 import numpy as np
 from scipy import stats
 
 class truncated_normal():
-    def __init__(self, a, b, known_var):
+    def __init__(self, a, b, var):
         self.a = a
         self.b = b
-        self.known_var = known_var
-
-    def sample_arm_i(self,rewards):
-        n = len(rewards)
+        self.know_var = var
+    def sample_arm_i(self,reward):
+        n = len(reward)
         if n == 0:
             return stats.uniform.rvs(loc=self.a, scale=self.b)
         else:
-            mu0 = np.mean(rewards)
-            sigma0 = np.sqrt(self.known_var/n)
-            return stats.truncnorm.rvs(self.a, self.b, loc=mu0, scale=sigma0)
+            mu0 = np.mean(reward)
+            sigma0 = np.sqrt(self.know_var/n)
+            a_norm = (self.a - mu0) / sigma0
+            b_norm = (self.b - mu0) / sigma0
+            return stats.truncnorm.rvs(a_norm, b_norm, loc=mu0, scale=sigma0)
 
     def mean(self, rewards):
         n = len(rewards)
         if n == 0:
-            return (self.b - self.a)/2.0
+            return (self.b - self.a) / 2.0
         else:
             mu0 = np.mean(rewards)
-            sigma0 = np.sqrt(self.known_var/n)
-            return stats.truncnorm.mean(self.a, self.b, loc=mu0, scale=sigma0)
+            sigma0 = np.sqrt(self.know_var / n)
+            a_norm = (self.a - mu0) / sigma0
+            b_norm = (self.b - mu0) / sigma0
+            return stats.truncnorm.mean(a_norm, b_norm, loc=mu0, scale=sigma0)
