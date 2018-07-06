dbbde29e3269d4184a8b7f59086665d56d7bead7
Pieter Libin <pieter.libin@vub.ac.be>
2018-07-06 12:17:43 +0200

    fix the t-distribution

--- posteriors/T_distribution_Posterior.py
+++ posteriors/T_distribution_Posterior.py
@@ -1,18 +1,18 @@
 import numpy as np
 class T_Posterior():
-
     def __init__(self,alpha):
         self.alpha = alpha
 
+    def freedom(n):
+        n+(2*self.alpha)-1
+
     def sample_arm_i(self,reward):
         n = len(reward)
-        if n == 1:
-            T = reward[0]
-        else:
-            sigma = np.std(reward)
-            mu = np.mean(reward)
-            T = mu + np.random.standard_t(n+(2*self.alpha)-1)*sigma
-        return T
+        freedom = self.freedom(n)
+        sigma = np.sqrt(np.var(reward)/freedom)
+        mu = np.mean(reward)
+        sample = np.random.standard_t(freedom)
+        return mu + sample*sigma
 
     def mean(self, rewards):
         return np.mean(rewards)
