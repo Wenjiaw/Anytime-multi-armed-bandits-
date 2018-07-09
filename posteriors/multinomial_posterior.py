76f626f4e7382f2d50c2c7149d63adb1e176e011
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-07-09 13:30:15 +0200

    fix

--- posteriors/multinomial_posterior.py
+++ posteriors/multinomial_posterior.py
@@ -1,21 +1,18 @@
 import numpy as np
 class MultinomialPosterior():
-    def __init__(self, variance, var0, mu0):
-        self.known_var = variance
-        self.var0 = var0
-        self.mu0 = mu0
+    def __init__(self, alpha):
+        self.alpha = alpha
 
     def sample_arm_i(self, reward):
-        if len(reward) == 0:
-            return 0
-        else:
-            unique, counts = np.unique(reward, return_counts=True)
-            p_list = np.random.dirichlet(counts)
-            return np.dot(unique, p_list)
+
+        unique, counts = np.unique(reward, return_counts=True)
+        p_list = np.random.dirichlet(self.alpha)
+        x = np.dot(unique, counts)
+        return np.dot(x, p_list)
+
 
     def mean(self, rewards):
-        if len(rewards) == 0:
-            return 0
-        else:
-            unique, counts = np.unique(rewards, return_counts=True)
-            return np.dot(unique,counts/len(rewards))
+
+        unique, counts = np.unique(rewards, return_counts=True)
+        x =np.dot(unique, counts)
+        return np.dot(x,self.alpha)/sum(self.alpha)
