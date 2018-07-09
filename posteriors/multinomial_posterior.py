9b67eb3e6d918d9c61a95a8f748cc74fa38021e2
Wenjiaw <33827874+Wenjiaw@users.noreply.github.com>
2018-07-09 14:43:37 +0200

    fix mean

--- posteriors/multinomial_posterior.py
+++ posteriors/multinomial_posterior.py
@@ -7,12 +7,10 @@ class MultinomialPosterior():
 
         unique, counts = np.unique(reward, return_counts=True)
         p_list = np.random.dirichlet(self.alpha)
-        x = np.dot(unique, counts)
-        return np.dot(x, p_list)
+        return np.dot(counts, p_list)
 
 
     def mean(self, rewards):
 
         unique, counts = np.unique(rewards, return_counts=True)
-        x =np.dot(unique, counts)
-        return np.dot(x,self.alpha)/sum(self.alpha)
+        return np.dot(count,self.alpha)/sum(self.alpha)
